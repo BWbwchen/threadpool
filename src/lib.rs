6aa1c28ccd240d21b78169ca864c3bf27938d6c3
BWbwchen <tim.chenbw@gmail.com>
2024-04-05 12:16:13 +0800

    [feat 	]: basic thread pool with `Arc<Mutex<channel>>`
    Signed-off-by: Bo-Wei Chen(BWbwchen) <tim.chenbw@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,27 @@
 //! ThreadPool
 //! A ultra simple and lightweight thread pool implementation.
+
+use std::{
+    sync::{
+        mpsc::{channel, Receiver, Sender},
+        Arc, Mutex,
+    },
+    thread::{self, JoinHandle},
+};
 #[deny(missing_docs)]
 
+type ThreadJobType = ();
+type JobType = Box<
+    dyn FnOnce() -> ThreadJobType // closure
+        + Send // safely pass the &mut closure between threads.
+        + 'static, // we don't know how much time that this closure will execute.
+>;
+
 /// Thread Pool object.
-pub struct ThreadPool;
+pub struct ThreadPool {
+    job_sender: Sender<JobType>,
+    workers: Vec<Worker>,
+}
 
 impl ThreadPool {
     /// Create a new ThreadPool with maximum `nthreads` threads.
@@ -12,16 +30,45 @@ impl ThreadPool {
     /// if the `nthreads` is zero, it will panic.
     pub fn new(nthreads: usize) -> Self {
         assert!(nthreads > 0);
-        ThreadPool {}
+        let mut workers = Vec::with_capacity(nthreads);
+
+        let (job_sender, receiver) = channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        for id in 0..nthreads {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+
+        ThreadPool {
+            job_sender,
+            workers,
+        }
     }
 
     pub fn spawn<F>(&self, f: F)
     where
-        F: FnOnce() // closure
+        F: FnOnce() -> ThreadJobType // closure
             + Send // safely pass the &mut closure between threads.
             + 'static, // we don't know how much time that this closure will execute.
     {
-        todo!()
+        self.job_sender.send(Box::new(f)).unwrap();
+    }
+}
+
+// Worker thread
+struct Worker {
+    id: usize,
+    thread: JoinHandle<ThreadJobType>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<Receiver<JobType>>>) -> Self {
+        let thread = thread::spawn(move || loop {
+            let job = receiver.lock().unwrap().recv().unwrap();
+            println!("Thread {} is doing jobs...", &id);
+            job();
+        });
+        Worker { id, thread }
     }
 }
 
@@ -32,6 +79,6 @@ mod tests {
     #[test]
     #[should_panic]
     fn test_panic_new() {
-        let zero_size_thread_pool = ThreadPool::new(0);
+        let _ = ThreadPool::new(0);
     }
 }
